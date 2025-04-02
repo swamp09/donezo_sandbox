b8217c36e48114d2487d6525406d44c014e6b6c1
swamp09 <shu.numata@esm.co.jp>
2025-04-02 16:41:39 +0900

    デザイン by cline with claude

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,43 @@
+class TasksController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_task, only: [:update, :destroy]
+
+  def index
+    @tasks = current_user.tasks.order(created_at: :desc)
+  end
+
+  def create
+    @task = current_user.tasks.build(task_params)
+    if @task.save
+      @tasks = current_user.tasks.order(created_at: :desc)
+      render turbo_stream: [
+        turbo_stream.replace("tasks", partial: "tasks/tasks", locals: { tasks: @tasks }),
+        turbo_stream.replace("task_form", partial: "tasks/form")
+      ]
+    else
+      render :index, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    @task.update(task_params)
+    @tasks = current_user.tasks.order(created_at: :desc)
+    render turbo_stream: turbo_stream.replace("tasks", partial: "tasks/tasks", locals: { tasks: @tasks })
+  end
+
+  def destroy
+    @task.destroy
+    @tasks = current_user.tasks.order(created_at: :desc)
+    render turbo_stream: turbo_stream.replace("tasks", partial: "tasks/tasks", locals: { tasks: @tasks })
+  end
+
+  private
+
+  def set_task
+    @task = current_user.tasks.find(params[:id])
+  end
+
+  def task_params
+    params.require(:task).permit(:title, :completed)
+  end
+end
