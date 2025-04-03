c0fa9428912199c4b4cd576112a564498dfb7653
swamp09 <shu.numata@esm.co.jp>
2025-04-03 14:51:18 +0900

    テストをパスさせるため、不要なテストを削除したりfixturesを整えたりするなどした

--- test/controllers/tasks_controller_test.rb
+++ test/controllers/tasks_controller_test.rb
@@ -1,23 +1,17 @@
+# frozen_string_literal: true
+
 require 'test_helper'
 
 class TasksControllerTest < ActionDispatch::IntegrationTest
+  include Devise::Test::IntegrationHelpers
+
+  setup do
+    @user = users(:alice)
+    sign_in @user
+  end
+
   test 'should get index' do
-    get tasks_index_url
-    assert_response :success
-  end
-
-  test 'should get create' do
-    get tasks_create_url
-    assert_response :success
-  end
-
-  test 'should get update' do
-    get tasks_update_url
-    assert_response :success
-  end
-
-  test 'should get destroy' do
-    get tasks_destroy_url
+    get tasks_url
     assert_response :success
   end
 end
